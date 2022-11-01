a17b5eda74f8c23cb8970488aa346d74fbbf2864
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-10-31 22:22:45 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>GoPizza</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
